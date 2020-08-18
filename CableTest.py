4bf11e3c744c4c1cbf58b7f847251918f01737e0
J Watts <44657478+5voltsgc@users.noreply.github.com>
2020-08-18 10:14:11 -0600

    Add files via upload
    working program

--- CableTest.py
+++ CableTest.py
@@ -35,7 +35,7 @@ SleepTimeL = .75
 units_under_test = "testing "+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 test_count = 0
 insulation_limit = 5.0e+6  #5Meg or 5,000,000 ohms
-
+base_line_samples = 2
 
 
 uut_name = input("Please enter name of sample to be tested:")
@@ -71,14 +71,15 @@ def relays_on():
         GPIO.output(pin_list[l],GPIO.LOW)
 
 
-#ResTest()
-        
+#ResTest()  
 def res_test():
     global test_count
     global print_var
+    global bsline
     
     res_test_pass = True
     test_count += 1
+    #print var is erased and the first two columns added
     print_var = str(datetime.datetime.now()) + "," + str(test_count)+ ","
     
     for m in range(num_conductors):
@@ -87,16 +88,57 @@ def res_test():
         time.sleep(SleepTimeL)
         my_instrument.write('MEAS:FRES? 100 OHM')
         res_reading[m] = float(my_instrument.read_bytes(15))
-        print_var = print_var + str(round(res_reading[m]),4) + ","
+        
+        print_var = print_var + str(round(res_reading[m],4)) + ","
         
         #add the pass fail when base line is done
-#         if 2 , (res_reading[m] - base_line[m]):
-#             res_test_pass = False
+        if 2 < (res_reading[m] - bsline[m]):
+            res_test_pass = False
             
         relays_off()
     print_var = print_var + str(res_test_pass) +  ","
-    print(print_var)
 
+
+#Baseline Test()  
+def base_line():
+    baseline_sample = np.empty([num_conductors,base_line_samples])
+
+    for o in range(0,base_line_samples):
+                
+        for n in range(num_conductors):
+            GPIO.output(pin_list[n],GPIO.LOW)
+            GPIO.output(pin_list[15-n] ,GPIO.LOW)
+            
+            #setting time for relays
+            time.sleep(SleepTimeL)
+            my_instrument.write('MEAS:FRES? 100 OHM')
+            baseline_sample[n,o] = float(my_instrument.read_bytes(15))
+            
+            relays_off()
+        
+        # Insulation Test    
+    for e in range(num_conductors):
+        #Turn on A side relay
+        GPIO.output(pin_list[e],GPIO.LOW)
+        
+        #Turn on all B side relays
+        for r in range(8, 16):
+            GPIO.output(pin_list[r],GPIO.LOW)
+        
+        # Turn off matching relay for Cross talk check
+        GPIO.output(pin_list[15-e],GPIO.HIGH)
+        
+        #give time for relays to make contact before measurment
+        time.sleep(SleepTimeL)
+
+        my_instrument.write('MEAS:FRES? 100000000 OHM') #100M Ohms
+        ins_reading[e] = float(my_instrument.read_bytes(15))
+    
+        relays_off()
+        
+
+    global bsline
+    bsline = baseline_sample.mean(axis=1)
 
 
 def insulation_test():
@@ -129,7 +171,8 @@ def insulation_test():
         
         relays_off() 
     print_var = print_var + str(ins_test_pass)
-    print(print_var)
+
+
 def turn_on_relays (relay_pin):    
 
     if not GPIO.input(pin_list[relay_pin]): #True is relay off low level
@@ -155,5 +198,47 @@ def relay_check():
             else:
                 print("Please enter a number between 0 - 15")
 
-insulation_test()
+def results():
+   
+    with open(filename, 'a') as f:
+ 
+        if (test_count ==1):
+            f.write(log_header)
+            f.write('\n')
+            
+        f.write(print_var)
+        f.write('\n')
+        f.close
+        print(print_var)
+        
+    
+try:
+    base_line()#need to add the insulation check
+    print(bsline)
+    print(ins_reading)
+    
+    X = input("Are these baseline values expected? [y/n]: ")
+    
+    run = False
+    if (X == "y" or X == "Y"):
+        run = True
+        
+    print("Begining testing to stop press CTRL C")
+    print("The log filename will be: " + str(filename))
+    print(log_header)
+    while run == True:
+        res_test()
+        insulation_test()
+        results()
+
+
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    GPIO.cleanup() # cleanup all GPIO    
+    print("Log file name is: " + str(filename))
+    
+    
+    
+
+
+
 GPIO.cleanup()
